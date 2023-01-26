1893b92d4e1d070e059b2621a9bdec2a2bc2ffc3
Hongtsewen <tsewen.hong@gmail.com>
2023-01-27 07:46:42 +0800

    add BAT2006 files

---
+++ BAT2006/yiconv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+iconv -f BIG-5 -t UTF-8 -o day10.html day10.html
+iconv -f BIG-5 -t UTF-8 -o day1.html day1.html
+iconv -f BIG-5 -t UTF-8 -o day2.html day2.html
+iconv -f BIG-5 -t UTF-8 -o day3.html day3.html
+iconv -f BIG-5 -t UTF-8 -o day4.html day4.html
+iconv -f BIG-5 -t UTF-8 -o day5.html day5.html
+iconv -f BIG-5 -t UTF-8 -o day6.html day6.html
+iconv -f BIG-5 -t UTF-8 -o day7.html day7.html
+iconv -f BIG-5 -t UTF-8 -o day8.html day8.html
+iconv -f BIG-5 -t UTF-8 -o day9.html day9.html
+iconv -f BIG-5 -t UTF-8 -o dayend.html dayend.html
+iconv -f BIG-5 -t UTF-8 -o index.html index.html
